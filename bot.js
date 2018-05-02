77ecae6cb0bc6ed75194b43ff8cb1a57849db7f3
Omertis <36013948+Omertis@users.noreply.github.com>
2018-05-02 15:41:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -264,6 +264,23 @@ msg.author.send("Commands Music " + `  **
 
 
 
+client.on('ready', function(){
+    var ms = 60000 ;
+    var setGame = ['$help | $دعوه','${client.guilds.size} server|${client.users.size} Users'];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/Bull`);
+    }, ms);
+
+});
 
 
 

d368f088cb9459edb16a80027c94610081c2ddf8
Omertis <36013948+Omertis@users.noreply.github.com>
2018-05-02 15:26:33 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.TOKEN = 'NDQwNjU5NDI4MDEwODg1MTUx.Dck7lw.lkN6HrgZaaCMSR5vajkYvQlszcY'; // ya wr3 la tns5
+
+exports.PREFIX = '$';
+
+exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
